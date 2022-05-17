937b975a97ae4e9b46d17a3f81d9e761ec4edfef
Leo-ui-582 <lipovetskiy88@mail.ru>
2022-05-17 10:26:29 +0300

    fixed

--- pw1/3.py
+++ pw1/3.py
@@ -1,7 +1,11 @@
-def byte(a):
-    try:
-        c = eval("str.encode(a, encoding='utf-8')")
-    except SyntaxError:
-        print('no')
+VAR_1 = 'attribute'
+VAR_2 = 'класс'
+VAR_3 = 'функция'
+VAR_4 = 'type'
 
-byte('класс')
+WORDS = [VAR_1, VAR_2, VAR_3, VAR_4]
+for word in WORDS:
+    try:
+        bytes(word, 'ascii')
+    except UnicodeEncodeError:
+        print(f'word" {word}" can not write in bytes string')
